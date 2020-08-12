76cebfcc6b44a1797f0f3f7477f31ebbf8acf0e8
xiaocaiji945 <412278432@qq.com>
2020-08-12 16:08:25 +0800

    work pytest01

--- work/work_pytest01/test_case/test_caculator.py
+++ work/work_pytest01/test_case/test_caculator.py
@@ -3,7 +3,6 @@ import yaml
 from work.work_pytest01.pythoncode.calculator import Calculator
 
 
-
 # 读取测试数据
 
 
@@ -11,31 +10,35 @@ class GetDatas:
     with open('./datas/calculator.yml', encoding='utf-') as f:
         mydatas = yaml.safe_load(f)
 
-
     def add_datas(self):
         adddatas = self.mydatas['add']['datas']
         addname = self.mydatas['add']['myids']
         return [adddatas, addname]
+
     def sub_datas(self):
         subdatas = self.mydatas['sub']['datas']
         subname = self.mydatas['sub']['myids']
         return [subdatas, subname]
+
     def mul_datas(self):
         muldatas = self.mydatas['mul']['datas']
         mulname = self.mydatas['mul']['myids']
         return [muldatas, mulname]
+
     def div_datas(self):
         divdatas = self.mydatas['div']['datas']
         divname = self.mydatas['div']['myids']
         return [divdatas, divname]
 
+
 class TestCaculator:
 
     def setup_class(self):
         self.calc = Calculator()
-        print("\n******测试开始******\n")
+        print("\n**********测试开始**********\n")
+
     def teardown_class(self):
-        print("\n******测试结束******")
+        print("\n**********测试结束**********")
 
     def setup(self):
         print("~~~~~~开始计算~~~~~~")
